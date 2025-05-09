9551eee45fcd502809bc5ebdf2ac5a6eb8fe6a3a
DPChanny <kyoungchan0516@gmail.com>
2025-05-09 20:08:56 +0900

    env adjustment

--- fastapi/app/sc3.py
+++ fastapi/app/sc3.py
@@ -1,16 +1,16 @@
 import boto3
 from envs import (
-    AWS_ACCESS_KEY_ID,
+    AWS_ACCESS_KEY,
     AWS_REGION,
-    AWS_SECRET_ACCESS_KEY,
-    S3_BUCKET_NAME,
+    AWS_SECRET_KEY,
+    AWS_BUCKET_NAME,
 )
 
 
 s3_client = boto3.client(
     "s3",
-    aws_access_key_id=AWS_ACCESS_KEY_ID,
-    aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+    aws_access_key_id=AWS_ACCESS_KEY,
+    aws_secret_access_key=AWS_SECRET_KEY,
     region_name=AWS_REGION,
 )
 
@@ -19,7 +19,7 @@ def get_presigned_url(key):
     return s3_client.generate_presigned_url(
         "put_object",
         Params={
-            "Bucket": S3_BUCKET_NAME,
+            "Bucket": AWS_BUCKET_NAME,
             "Key": key,
             "ContentType": "image/jpeg",
         },
